6441cb34e5e27c4e2fa93fc8d9105297fa4fc0cf
Ajith178 <ajithnirmal171097@gmail.com>
2022-08-20 20:35:36 +0530

    eval

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+  const [value,setvalue] = useState("");
+  const gettingValues = (e)=>
+  {
+     setvalue(value.concat(e.target.value))
+  }
+  const final = ()=>{
+    setvalue(eval(value))
+  }
+  const clear = ()=>{
+    setvalue("")
+  }
+  return(<>
+  <div className='calc'>
+    <h2>Calculator</h2>
+    <div className='grid1'>
+  <form >
+  <input type="text" value={value}></input>  
+  <button onClick={clear} id="clear">C</button>
+   </form>
+  
+   </div>
+   <div className='left'>
+   <div className='grid'>
+  <button onClick={(e)=>gettingValues(e)} value="1">1</button>
+  <button onClick={(e)=>gettingValues(e)} value="2">2</button>
+  <button onClick={(e)=>gettingValues(e)} value="3">3</button>
+  <button onClick={(e)=>gettingValues(e)} value="/">/</button>
+  <button onClick={(e)=>gettingValues(e)} value="4">4</button>
+  <button onClick={(e)=>gettingValues(e)} value="5">5</button>
+  <button onClick={(e)=>gettingValues(e)} value="6">6</button>
+  <button onClick={(e)=>gettingValues(e)} value="-">-</button>
+  <button  onClick={(e)=>gettingValues(e)} value="7">7</button>
+  <button  onClick={(e)=>gettingValues(e)} value="8">8</button>
+  <button  onClick={(e)=>gettingValues(e)} value="9">9</button>
+  <button onClick={(e)=>gettingValues(e)} value="+">+</button>
+  <button  onClick={(e)=>gettingValues(e)} value=".">.</button>
+  <button  onClick={(e)=>gettingValues(e)} value="0">0</button>
+  <button onClick={(e)=>gettingValues(e)} value="*">*</button>
+  <button onClick={final}>=</button>
+  
+  </div>
+  </div>
+  </div>
+  </>)
 }
 
 export default App;
